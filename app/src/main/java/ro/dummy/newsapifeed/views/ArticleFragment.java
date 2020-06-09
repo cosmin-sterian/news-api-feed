46db3f0edf8c7a459a10cc19b29d43b8d947c295
Cosmin-Cristian Sterian <c.cristian.sterian@gmail.com>
2020-06-09 21:33:34 +0300

    removed dummy content generated by android studio

--- app/src/main/java/ro/dummy/newsapifeed/views/ArticleFragment.java
+++ app/src/main/java/ro/dummy/newsapifeed/views/ArticleFragment.java
@@ -1,29 +1,22 @@
 package ro.dummy.newsapifeed.views;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-import ro.dummy.newsapifeed.R;
 import ro.dummy.newsapifeed.data.Article;
 import ro.dummy.newsapifeed.databinding.FragmentArticleListBinding;
-import ro.dummy.newsapifeed.views.dummy.DummyContent;
 
 /**
  * A fragment representing a list of Articles.
