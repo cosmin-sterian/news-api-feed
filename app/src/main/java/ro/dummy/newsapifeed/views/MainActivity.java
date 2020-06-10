9612d22180bec755156194fdc909cd4ebad64d67
Cosmin-Cristian Sterian <c.cristian.sterian@gmail.com>
2020-06-11 01:44:58 +0300

    added BottomNavigationView and icons; changed app icon

--- app/src/main/java/ro/dummy/newsapifeed/views/MainActivity.java
+++ app/src/main/java/ro/dummy/newsapifeed/views/MainActivity.java
@@ -1,16 +1,64 @@
 package ro.dummy.newsapifeed.views;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import ro.dummy.newsapifeed.R;
 
 public class MainActivity extends AppCompatActivity {
 
+	private BottomNavigationView bottomNavigationView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+			getWindow().setNavigationBarDividerColor(getResources().getColor(R.color.colorPrimary, getTheme()));
+		}
+
+		bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+		bottomNavigationView.setItemIconTintList(null);
+//		bottomNavView.getMenu().findItem(R.id.menu_item_top_headlines).setIcon(R.mipmap.ic_top_headlines);
+
+		bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+			@Override
+			public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+				switch (item.getItemId()) {
+					case R.id.menu_item_top_headlines:
+						Log.d("sda", "top_headlines_pressed");
+//						item.setIcon(R.drawable.ic_burning_newspaper_compressed);
+						return true;
+					case R.id.menu_item_all_news:
+						Log.d("sda", "all_news_pressed");
+//						deselectTopHeadlines();
+						return true;
+					default:
+						return true;
+				}
+			}
+		});
 	}
+
+//	private void deselectTopHeadlines() {
+//		bottomNavigationView
+//				.getMenu()
+//				.findItem(R.id.menu_item_top_headlines)
+//				.setIcon(R.drawable.ic_top_headers);
+//	}
+//
+//	private void selectTopHeadlines() {
+//		MenuItem item = bottomNavigationView
+//				.getMenu()
+//				.findItem(R.id.menu_item_top_headlines)
+//				.setIcon(R.drawable.ic_burning_newspaper_compressed);
+//	}
 }
